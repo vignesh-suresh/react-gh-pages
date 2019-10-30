a81dc8a2fcf8c4403778ab163cbd58a35c824878
aravind <aravind.s@perfios.com>
2019-10-30 14:52:49 +0530

    Added json config

---
+++ build/precache-manifest.db62541bd4f6d57d225ef9b7e8b17171.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "360f643acff2ad3c7ac4ec2fa65b4ffb",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "3ded4858b083e223fef1",
+    "url": "/react-gh-pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3d55610d57f671e83d66",
+    "url": "/react-gh-pages/static/js/2.437d1e27.chunk.js"
+  },
+  {
+    "revision": "3ded4858b083e223fef1",
+    "url": "/react-gh-pages/static/js/main.c19bf8e2.chunk.js"
+  },
+  {
+    "revision": "d1ce432b67083ccc0594",
+    "url": "/react-gh-pages/static/js/runtime-main.460020dd.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-gh-pages/static/media/logo.25bf045c.svg"
+  }
+]);
